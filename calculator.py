33d20cd7a546e5fd7ba72979d0225b2c5f7e7690
Abhishekgowda K S <36053351+AbhishekgowdaKS@users.noreply.github.com>
2018-06-30 15:57:06 +0530

    calculator first impression is uploaded

---
+++ calculator.py
@@ -0,0 +1,22 @@
+a=float(input('number 1 : '))
+y=float(input('number 2 : '))
+print('select the operation : \n1.addition\n2.subtraction\n3.multiplication\n4.division\n5.Exit')
+ch=input('option : ')
+while(ch!='5'):
+    if ch=='1':
+        add=a+b
+        print(a,'+',b,'=',add)
+    elif ch=='2':
+        sub=a-b
+        print(a,'-',b,'=',sub)
+    elif ch=='3':
+        mul=a*b
+        print(a,'*',b,'=',mul)
+    elif ch=='4':
+        div=a/b
+        print(a,'/',b,'=',div)
+    else:
+        print('select correct option')
+    ch=input('option : ')
+print('thank you')
+
